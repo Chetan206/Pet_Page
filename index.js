c25ac73756de13a051a441d43a38fb649af2921c
Chetan_Developer <Chetanbansal664@gmail.com>
2025-07-04 09:14:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+document.getElementById("bn").innerHTML;
+
+function onClickBuyNow(){
+    alert("Not Available In this time");
+}
+
+function brono(id){
+       id.innerHTML = "I brave and playful dog";
+}
+
+function addToCart(id){
+    id.innerHTML = "Thank You Dear!";
+}
+
